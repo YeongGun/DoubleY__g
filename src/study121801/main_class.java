4aba5ccc4eb8b82cdd272897a97bb41cb86bc659
YoeongGun <yo941@naver.com>
2023-12-18 10:27:48 +0900

    1213

--- src/study121801/main_class.java
+++ src/study121801/main_class.java
@@ -4,5 +4,13 @@ public class main_class {
 
 	public static void main(String[] args) {
 		// TODO auto-generated method stub
+		// 자바스크립트 - let a = new Array();
+		//            let b = [10,20,30,40];
+		// p.push(3123); b.push(233);
+		
+		int[] arrl = {101,20};
+		
+		System.out.println( arr1[0]) );
+	
 	}
 }
